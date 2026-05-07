63d8c9d853d9c42f0368cabaf3314b10ab067bdb
K Yamaguchi | Web Dev in Training <k.yamaguchi.dev@gmail.com>
2026-05-07 14:40:54 +0900

    Add files via upload

---
+++ C#-practice/0507/ConPrime/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, ConPrime");
+
+for(int i=2;i<=100;i++){
+    bool n = true;
+    for(int j = 2;j<i;j++){
+        if(i%j==0){
+            n = false;;
+        }
+    }
+    if(n){
+        Console.WriteLine(i);
+    }
+    
+}
